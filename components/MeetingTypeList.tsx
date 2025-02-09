d644dd8756dbeac9650aa0679b50597d20a33460
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-09 19:45:44 +0530

    schedule the meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -7,6 +7,8 @@ import MeetingModel from "./MeetingModel";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "@/hooks/use-toast";
+import { Textarea } from "./ui/textarea";
+import ReactDatePicker from "react-datepicker";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -69,6 +71,10 @@ const MeetingTypeList = () => {
     }
   };
 
+
+const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
+
+
   return (
     <section
       className="grid grid-cols-1 gap-5
@@ -102,6 +108,61 @@ const MeetingTypeList = () => {
         handleClick={() => setMeetingsState("isJoiningMeeting")}
         className="bg-yellow-1"
       />
+
+      {!callDetails ? (
+        <MeetingModel
+          isOpen={meetingState === "isScheduleMeeting"}
+          onClose={() => setMeetingsState(undefined)}
+          title="Create Meeting"
+          handleClick={createMeeting}
+        >
+          <div className="flex flex-col gap-2.5">
+            <label className="text-based text-normal leading-[22px] text-sky-2">
+              Add a description
+            </label>
+
+            <Textarea
+              className="border-non bg-dark-3 focus-visible:ring-0
+            focus-visible:ring-offset-0"
+              onChange={(e) => {
+                setvalues({ ...values, description: e.target.value });
+              }}
+            />
+          </div>
+
+          <div className="flex w-full flex-col gap-2.5">
+            <label className="text-based text-normal leading-[22px] text-sky-2">
+              Select date and time
+            </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => {
+                setvalues({ ...values, dateTime: date! });
+              }}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+            />
+          </div>
+        </MeetingModel>
+      ) : (
+        <MeetingModel
+          isOpen={meetingState === "isScheduleMeeting"}
+          onClose={() => setMeetingsState(undefined)}
+          title="Meeting Created"
+          className="text-center"
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLink)
+            toast({title:"Link copied"})
+          }}
+          image="/icons/checked.svg"
+          buttonIcon="/icons/copy.svg"
+          buttonText="Copy Meeting Link"
+        />
+      )}
 
       <MeetingModel
         isOpen={meetingState === "isInstanMeeting"}
